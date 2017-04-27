133ad8ace3fee8fc571c9b0f841bfd4de6d86b8c
ElenaMGR <elenamgr@correo.ugr.es>
2017-04-27 18:46:55 +0200

    Practica 2

---
+++ practica2/build/my_astar_planner/CMakeFiles/my_astar_planner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_astar_planner.dir/src/myAstarPlanner.cpp.o"
+  "/home/elena/GradoInformatica/Tercero/2Cuatrimestre/TSI/Practicas/practica2/devel/lib/libmy_astar_planner.pdb"
+  "/home/elena/GradoInformatica/Tercero/2Cuatrimestre/TSI/Practicas/practica2/devel/lib/libmy_astar_planner.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_astar_planner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ practica2/build/my_astar_planner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/elena/GradoInformatica/Tercero/2Cuatrimestre/TSI/Practicas/practica2/src/my_astar_planner
+# Build directory: /home/elena/GradoInformatica/Tercero/2Cuatrimestre/TSI/Practicas/practica2/build/my_astar_planner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
