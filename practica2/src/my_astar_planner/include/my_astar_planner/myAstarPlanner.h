1fd814c34f1443904711de1217478ce16adf4f96
ElenaMGR <elenamgr@correo.ugr.es>
2017-05-04 10:37:27 +0200

    Practica 2- Modificación formato

--- practica2/src/my_astar_planner/include/my_astar_planner/myAstarPlanner.h
+++ practica2/src/my_astar_planner/include/my_astar_planner/myAstarPlanner.h
@@ -64,134 +64,135 @@
 
 
 namespace myastar_planner{
-  /**
+   /**
    * @class myastar_planner
    * @brief Provides an a-star simple global planner that will compute a valid goal point for the local planner by using classical astar implementation.
    */
 
    using namespace std;
-/**
- * @struct coupleOfCells
- * @brief A struct that represents a node, that is, a couple of current and parent cells
- */
-struct coupleOfCells {
-	unsigned int index;
-	unsigned int parent;
-	double gCost;
-	double hCost;
-	double fCost;
+   /**
+   * @struct coupleOfCells
+   * @brief A struct that represents a node, that is, a couple of current and parent cells
+   */
+   struct coupleOfCells {
+      unsigned int index;
+      unsigned int parent;
+      double gCost;
+      double hCost;
+      double fCost;
+   };
+
+
+   class MyastarPlanner : public nav_core::BaseGlobalPlanner { //implementa la interfaz que provee nav_core::BaseGlobalPlanner
+      public:
+         /**
+         * @brief  Constructor for the MyastarPlanner
+         */
+         MyastarPlanner();
+         /**
+         * @brief  Constructor for the MyastarPlanner
+         * @param  name The name of this planner
+         * @param  costmap_ros A pointer to the ROS wrapper of the costmap to use for planning
+         */
+         MyastarPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+         /**
+         * @brief  Initialization function for MyastarPlanner
+         * @param  name The name of this planner
+         * @param  costmap_ros A pointer to the ROS wrapper of the costmap to use for planning
+         */
+         void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+         /**
+          * @brief Given a goal pose in the world, compute a plan
+          * @param start The start pose
+          * @param goal The goal pose
+          * @param plan The plan... filled by the planner
+          * @return True if a valid plan was found, false otherwise
+          */
+          bool makePlan(const geometry_msgs::PoseStamped& start,
+             const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan);
+
+      private:
+
+         //necesarios para manejar el costmap y el footprint.
+         costmap_2d::Costmap2DROS* costmap_ros_;
+         double step_size_, min_dist_from_robot_;
+         costmap_2d::Costmap2D* costmap_;
+
+         //para publicar el plan
+         ros::Publisher plan_pub_;
+
+
+         //necesarios para manejar las listas de abiertos y cerrados de astar.
+         list<coupleOfCells> openList; //!< the open list: it contains all the expanded cells (current cells)
+         list<coupleOfCells> closedList; //!< the closed list: contains the explored cells
+
+         /**
+         * @brief  Checks the legality of the robot footprint at a position and orientation using the world model
+         * @param x_i The x position of the robot
+         * @param y_i The y position of the robot
+         * @param theta_i The orientation of the robot
+         * @return
+         */
+         double footprintCost(double x_i, double y_i, double theta_i);
+         /**
+         * @brief  Calcula la estimación del costo de ir desde una casilla (definida por su indice) hasta otra (definida por su índice)
+         * @param start El índice de la casilla inicial
+         * @param end El índice de la casilla final
+         * @return
+         */
+         double calculateHCost(unsigned int start, unsigned int goal);
+
+         //Función para la comparación de la función heurística aplicada a dos nodos.
+         //Devuelve true si f(c1) < f(c2)
+         static bool compareFCost(coupleOfCells const &c1, coupleOfCells const &c2);
+
+         //devuelve celdas adyacentes a CellID que estén libres
+         vector <unsigned int> findFreeNeighborCell (unsigned int CellID);
+
+         /*******************************************************************************/
+         //Function Name: addNeighborCellsToOpenList
+         //Inputs: the open list, the neighbors Cells and the parent Cell
+         //Output: the open list updated
+         //Description: it is used to add the neighbor Cells to the open list
+         /*********************************************************************************/
+         void addNeighborCellsToOpenList(list<coupleOfCells> & OPL, vector <unsigned int> neighborCells, unsigned int parent, float gCostParent, unsigned int goalCell);
+
+         //devuelve elcosto de moverse desde una casilla "here" hasta otra casilla "there"
+         double getMoveCost(unsigned int here, unsigned int there);
+
+         void publishPlan(const std::vector<geometry_msgs::PoseStamped>& path);
+
+         /********VISUALIZAR ESPACIO DE BUSQUEDA *************************/
+
+         //para publicar puntos de abiertos y cerrados
+         ros::Publisher marker_Open_publisher;
+         ros::Publisher marker_Closed_publisher;
+         //para publicar las marcas de los goals
+         ros::Publisher marker_Goals_publisher;
+         //necesario para guardar la lista de puntos de abiertos y cerrados a visualizar como markers en rviz.
+         visualization_msgs::Marker markers_OpenList;
+         visualization_msgs::Marker markers_ClosedList;
+         //necesario para guardar las line_list que marcan los puntos objetivo recomendados.
+         visualization_msgs::Marker markers_Goals;
+
+         void inicializaMarkersPoints(visualization_msgs::Marker &marker, string ns, int id, float r, float g, float b);
+         void inicializaMarkersLine_List(visualization_msgs::Marker &marker, string ns, int id, float r, float g, float b);
+         void visualizaCoords(ros::Publisher where, visualization_msgs::Marker &marker, double x, double y);
+         void visualizaCoordsLineUp(ros::Publisher where, visualization_msgs::Marker &marker, double x, double y, double z);
+         void visualizaCelda(ros::Publisher where, visualization_msgs::Marker &marker, unsigned int cell);
+
+
+         void visualizaLista(ros::Publisher where, visualization_msgs::Marker &marker, vector<unsigned int> lista);
+         void limpiaMarkers(ros::Publisher where, visualization_msgs::Marker &marker);
+
+         /************FIN VISUALIZAR ESPACIO DE BUSQUEDA *******************************/
+
+
+
+         bool initialized_;
+   };
 };
 
-
-  class MyastarPlanner : public nav_core::BaseGlobalPlanner { //implementa la interfaz que provee nav_core::BaseGlobalPlanner
-    public:
-      /**
-       * @brief  Constructor for the MyastarPlanner
-       */
-      MyastarPlanner();
-      /**
-       * @brief  Constructor for the MyastarPlanner
-       * @param  name The name of this planner
-       * @param  costmap_ros A pointer to the ROS wrapper of the costmap to use for planning
-       */
-      MyastarPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
-
-      /**
-       * @brief  Initialization function for MyastarPlanner
-       * @param  name The name of this planner
-       * @param  costmap_ros A pointer to the ROS wrapper of the costmap to use for planning
-       */
-      void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
-
-      /**
-       * @brief Given a goal pose in the world, compute a plan
-       * @param start The start pose
-       * @param goal The goal pose
-       * @param plan The plan... filled by the planner
-       * @return True if a valid plan was found, false otherwise
-       */
-      bool makePlan(const geometry_msgs::PoseStamped& start,
-          const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan);
-
-    private:
-
-      //necesarios para manejar el costmap y el footprint.
-      costmap_2d::Costmap2DROS* costmap_ros_;
-      double step_size_, min_dist_from_robot_;
-      costmap_2d::Costmap2D* costmap_;
-
-      //para publicar el plan
-      ros::Publisher plan_pub_;
-
-
-      //necesarios para manejar las listas de abiertos y cerrados de astar.
-      list<coupleOfCells> openList; //!< the open list: it contains all the expanded cells (current cells)
-	  list<coupleOfCells> closedList; //!< the closed list: contains the explored cells
-
-      /**
-       * @brief  Checks the legality of the robot footprint at a position and orientation using the world model
-       * @param x_i The x position of the robot
-       * @param y_i The y position of the robot
-       * @param theta_i The orientation of the robot
-       * @return
-       */
-      double footprintCost(double x_i, double y_i, double theta_i);
-      /**
-       * @brief  Calcula la estimación del costo de ir desde una casilla (definida por su indice) hasta otra (definida por su índice)
-       * @param start El índice de la casilla inicial
-       * @param end El índice de la casilla final
-       * @return
-       */
-      double calculateHCost(unsigned int start, unsigned int goal);
-
-      //Función para la comparación de la función heurística aplicada a dos nodos.
-      //Devuelve true si f(c1) < f(c2)
-      static bool compareFCost(coupleOfCells const &c1, coupleOfCells const &c2);
-
-      //devuelve celdas adyacentes a CellID que estén libres
-      vector <unsigned int> findFreeNeighborCell (unsigned int CellID);
-
-      /*******************************************************************************/
-      //Function Name: addNeighborCellsToOpenList
-      //Inputs: the open list, the neighbors Cells and the parent Cell
-      //Output: the open list updated
-      //Description: it is used to add the neighbor Cells to the open list
-      /*********************************************************************************/
-      void addNeighborCellsToOpenList(list<coupleOfCells> & OPL, vector <unsigned int> neighborCells, unsigned int parent, float gCostParent, unsigned int goalCell);
-
-      //devuelve elcosto de moverse desde una casilla "here" hasta otra casilla "there"
-      double getMoveCost(unsigned int here, unsigned int there);
-
-      void publishPlan(const std::vector<geometry_msgs::PoseStamped>& path);
-
-      /********VISUALIZAR ESPACIO DE BUSQUEDA *************************/
-
-       //para publicar puntos de abiertos y cerrados
-      ros::Publisher marker_Open_publisher;
-      ros::Publisher marker_Closed_publisher;
-      //para publicar las marcas de los goals
-      ros::Publisher marker_Goals_publisher;
-      //necesario para guardar la lista de puntos de abiertos y cerrados a visualizar como markers en rviz.
-      visualization_msgs::Marker markers_OpenList;
-      visualization_msgs::Marker markers_ClosedList;
-      //necesario para guardar las line_list que marcan los puntos objetivo recomendados.
-      visualization_msgs::Marker markers_Goals;
-
-      void inicializaMarkersPoints(visualization_msgs::Marker &marker, string ns, int id, float r, float g, float b);
-      void inicializaMarkersLine_List(visualization_msgs::Marker &marker, string ns, int id, float r, float g, float b);
-      void visualizaCoords(ros::Publisher where, visualization_msgs::Marker &marker, double x, double y);
-      void visualizaCoordsLineUp(ros::Publisher where, visualization_msgs::Marker &marker, double x, double y, double z);
-      void visualizaCelda(ros::Publisher where, visualization_msgs::Marker &marker, unsigned int cell);
-
-
-      void visualizaLista(ros::Publisher where, visualization_msgs::Marker &marker, vector<unsigned int> lista);
-      void limpiaMarkers(ros::Publisher where, visualization_msgs::Marker &marker);
-
-      /************FIN VISUALIZAR ESPACIO DE BUSQUEDA *******************************/
-
-
-
-      bool initialized_;
-  };
-};
 #endif
